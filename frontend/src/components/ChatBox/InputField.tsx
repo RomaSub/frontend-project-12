3e23b754f357c5fcbe43d16eff8a32cda7f7da3e
Roma <118161260+RomaSub@users.noreply.github.com>
2024-08-06 19:43:34 +0300

    fix input

--- frontend/src/components/ChatBox/InputField.tsx
+++ frontend/src/components/ChatBox/InputField.tsx
@@ -1,9 +1,11 @@
+import { useFormik } from 'formik';
 import leoProfanity from 'leo-profanity';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import { Button, Form, InputGroup } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
+import * as yup from 'yup';
 import { useAddMessageMutation } from '../../services/messagesApi';
 import type { RootState } from '../../services/store';
 
@@ -16,31 +18,44 @@ export const InputField = ({ channelId }: InputFieldProps) => {
   const { t } = useTranslation();
   const username = useSelector((state: RootState) => state.auth.username);
   const [addMessage] = useAddMessageMutation();
-  const [body, setBody] = useState('');
+
+  const validationSchema = yup.object().shape({
+    body: yup.string().trim().required('Required'),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      body: '',
+    },
+    validationSchema,
+    onSubmit: async ({ body }) => {
+      try {
+        const filtredMessage = leoProfanity.clean(body);
+        const newMessage = { username, channelId, body: filtredMessage };
+
+        await addMessage(newMessage);
+      } catch (err) {
+        formik.setSubmitting(false);
+        console.error(err);
+        toast.error(t('toast.networkError'));
+      }
+    },
+  });
 
   useEffect(() => {
     inputRef.current?.focus();
-  }, [channelId]);
+  }, [channelId, formik.isSubmitting]);
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const filtredMessage = leoProfanity.clean(body);
-    const newMessage = { username, channelId, body: filtredMessage };
-    try {
-      await addMessage(newMessage);
-      setBody('');
-    } catch (err) {
-      toast.error(t('toast.networkError'));
-    }
-  };
+  const isInvalid = !formik.isValid || !formik.dirty;
 
   return (
     <div className='mt-auto px-5 py-3'>
-      <Form className='py-1 border rounded-2' onSubmit={e => handleSubmit(e)}>
-        <InputGroup>
+      <Form className='py-1 border rounded-2' onSubmit={formik.handleSubmit}>
+        <InputGroup hasValidation={isInvalid}>
           <Form.Control
-            value={body}
-            onChange={e => setBody(e.target.value)}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.body}
             className='border-0 p-0 ps-2'
             name='body'
             aria-label={t('chatPage.newMessage')}
@@ -48,7 +63,7 @@ export const InputField = ({ channelId }: InputFieldProps) => {
             required
             ref={inputRef}
           />
-          <Button type='submit' variant='group-vertical' disabled={body.trim() === ''}>
+          <Button type='submit' variant='group-vertical' disabled={isInvalid}>
             <i className='bi bi-arrow-right-square m-0 h5'></i>
             <span className='visually-hidden'>{t('chatPage.send')}</span>
           </Button>
