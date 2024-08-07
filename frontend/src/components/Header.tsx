a6a025bc86c4c70bd8cfd13f8fdc6cb2d021d2cd
Roma <118161260+RomaSub@users.noreply.github.com>
2024-08-07 03:24:05 +0300

    fix navbar

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,15 +2,15 @@ import { Container, Navbar } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import { getRoutes } from '../routes';
+import { LanguageSwitcher } from './LanguageSwitcher';
 import { LogoutButton } from './LogoutButton';
 import { ThemeSwitcher } from './ThemeSwitcher';
-import { LanguageSwitcher } from './LanguageSwitcher';
 
 const Header = () => {
   const { t } = useTranslation();
 
   return (
-    <Navbar expand='lg' className='shadow-sm'>
+    <Navbar expand='sm' className='shadow-sm'>
       <Container>
         <Navbar.Brand as={Link} to={getRoutes.chatPagePath()}>
           {t('header.chatLogo')}
